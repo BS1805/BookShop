3af4254974b3e71349c00f9a88d30f8fa7a601e4
Hasibul Hasan Hasib <hasib.hasan@brainstation-23.com>
2025-03-25 19:57:37 +0600

    AppDbContext class added

---
+++ BookShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+    }
+}
